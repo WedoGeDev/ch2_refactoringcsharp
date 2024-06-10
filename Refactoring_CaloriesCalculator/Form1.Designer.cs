e71514c4826ac5dd87b7c322bcf10581a14d8992
Omar García Espíritu <omar.garcia.mail@gmail.com>
2024-06-10 11:05:14 -0600

    Agregar archivos de proyecto.

---
+++ Refactoring_CaloriesCalculator/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Refactoring_CaloriesCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFeet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInches = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCalories = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtIdealWeigth = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.txtIdealWeigth);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtDistance);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtWeight);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtCalories);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtInches);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtFeet);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rbtnFemale);
+            this.groupBox1.Controls.Add(this.rbtnMale);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 220);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Location = new System.Drawing.Point(6, 19);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(73, 17);
+            this.rbtnMale.TabIndex = 0;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Masculino";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Location = new System.Drawing.Point(85, 19);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(71, 17);
+            this.rbtnFemale.TabIndex = 1;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Femenino";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Altura";
+            // 
+            // txtFeet
+            // 
+            this.txtFeet.Location = new System.Drawing.Point(9, 79);
+            this.txtFeet.Name = "txtFeet";
+            this.txtFeet.Size = new System.Drawing.Size(47, 20);
+            this.txtFeet.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Piés";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pulgadas";
+            // 
+            // txtInches
+            // 
+            this.txtInches.Location = new System.Drawing.Point(95, 79);
+            this.txtInches.Name = "txtInches";
+            this.txtInches.Size = new System.Drawing.Size(47, 20);
+            this.txtInches.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Años";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(9, 178);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(133, 20);
+            this.txtAge.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Edad";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(233, 19);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(142, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calcular";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cal.";
+            // 
+            // txtCalories
+            // 
+            this.txtCalories.Enabled = false;
+            this.txtCalories.Location = new System.Drawing.Point(233, 178);
+            this.txtCalories.Name = "txtCalories";
+            this.txtCalories.Size = new System.Drawing.Size(133, 20);
+            this.txtCalories.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(230, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Consumo diario recomendado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(148, 131);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Libras";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(9, 128);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(133, 20);
+            this.txtWeight.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Peso";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(372, 82);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Libras";
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Enabled = false;
+            this.txtDistance.Location = new System.Drawing.Point(233, 79);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(133, 20);
+            this.txtDistance.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(230, 62);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(113, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Distancia al peso ideal";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(372, 131);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(35, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Libras";
+            // 
+            // txtIdealWeigth
+            // 
+            this.txtIdealWeigth.Enabled = false;
+            this.txtIdealWeigth.Location = new System.Drawing.Point(233, 128);
+            this.txtIdealWeigth.Name = "txtIdealWeigth";
+            this.txtIdealWeigth.Size = new System.Drawing.Size(133, 20);
+            this.txtIdealWeigth.TabIndex = 21;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(230, 111);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(57, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Peso Ideal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 248);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Calculadora de Calorías";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInches;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFeet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCalories;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtIdealWeigth;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Refactoring_CaloriesCalculator/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Refactoring_CaloriesCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtDistance.Clear();
+            txtIdealWeigth.Clear();
+            txtCalories.Clear();
+
+            double result;
+            if (!double.TryParse(txtFeet.Text, out result))
+            {
+                MessageBox.Show("Altura (Pies) debe ser un valor numerico");
+                txtFeet.Select();
+                return;
+            }
+
+            if (!double.TryParse(txtInches.Text, out result))
+            {
+                MessageBox.Show("Altura (Pulgadas) debe ser un valor numerico");
+                txtInches.Select();
+                return;
+            }
+
+            if (!double.TryParse(txtWeight.Text, out result))
+            {
+                MessageBox.Show("Peso debe ser un valor numerico");
+                txtWeight.Select();
+                return;
+            }
+
+            if (!double.TryParse(txtAge.Text, out result))
+            {
+                MessageBox.Show("Edad debe ser un valor numerico");
+                txtAge.Select();
+                return;
+            }
+
+            if (!(Convert.ToDouble(txtFeet.Text) >= 5))
+            {
+                MessageBox.Show("La altura debe ser igual o mayor a 5 Piés");
+                txtFeet.Select();
+                return;
+            }
+
+            if (rbtnMale.Checked)
+            {
+                txtCalories.Text = (66
+                    + (6.3 * Convert.ToDouble(txtWeight.Text))
+                    + (12.9 * ((Convert.ToDouble(txtFeet.Text) * 12)
+                        + Convert.ToDouble(txtInches.Text)))
+                    - 6.8 * Convert.ToDouble(txtAge.Text)).ToString();
+                
+                txtIdealWeigth.Text = ((50 +
+                    (2.3 * (((Convert.ToDouble(txtFeet.Text) - 5) * 12)
+                    + Convert.ToDouble(txtInches.Text)))) * 2.2046).ToString();
+            }
+            else
+            {
+                txtCalories.Text = (655
+                    + (4.3 * Convert.ToDouble(txtWeight.Text))
+                    + (4.7 * ((Convert.ToDouble(txtFeet.Text) * 12)
+                        + Convert.ToDouble(txtInches.Text)))
+                    - 4.7 * Convert.ToDouble(txtAge.Text)).ToString();
+
+                txtIdealWeigth.Text = ((45.5 +
+                    (2.3 * (((Convert.ToDouble(txtFeet.Text) - 5) * 12)
+                    + Convert.ToDouble(txtInches.Text)))) * 2.2046).ToString();
+            }
+
+            txtDistance.Text = (Convert.ToDouble(txtWeight.Text)
+                - Convert.ToDouble(txtIdealWeigth.Text)).ToString();
+        }
+    }
+}
